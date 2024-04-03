d05634c2f50300d1f2de8babc8633b054e05fd7a
Dustin Tong <minhnhut.tong.goldenowl@gmail.com>
2024-04-03 22:16:32 +0700

    fix: fix ui create form

--- lib/src/features/create_form/cubit/create_form_cubit.dart
+++ lib/src/features/create_form/cubit/create_form_cubit.dart
@@ -4,31 +4,32 @@ import 'package:flutter_form_google/src/data/models/question.dart';
 import 'package:flutter_form_google/src/features/create_form/cubit/create_form_state.dart';
 
 class CreateFormCubit extends Cubit<CreateFormState> {
-  CreateFormCubit(MFormData? mFormData) : super(CreateFormState.ds(mFormData));
+  CreateFormCubit(MFormData? editFormData)
+      : super(CreateFormState.ds(editFormData));
 
   void addNewForm() {
-    List<MQuestion> updatedList = List.from(state.listFormData);
+    List<MQuestion> updatedList = List.from(state.questions);
     updatedList.add(MQuestion.empty());
-    emit(state.copyWith(listFormData: updatedList));
+    emit(state.copyWith(questions: updatedList));
   }
 
   void removeFormWithPosition(int position) {
-    List<MQuestion> updatedList = List.from(state.listFormData);
+    List<MQuestion> updatedList = List.from(state.questions);
     updatedList.removeAt(position);
-    emit(state.copyWith(listFormData: updatedList));
+    emit(state.copyWith(questions: updatedList));
   }
 
   void editFormData(MFormData mFormData) {
     emit(state.copyWith(
-        titleForm: mFormData.title, listFormData: mFormData.questions));
+        titleForm: mFormData.title, questions: mFormData.questions));
   }
 
   void isEditTitleForm() {
-    emit(state.copyWith(isEditTitile: true));
+    emit(state.copyWith(isEditTitle: true));
   }
 
   void onDoneChangeEditTitleForm() {
-    emit(state.copyWith(isEditTitile: false));
+    emit(state.copyWith(isEditTitle: false));
   }
 
   void onChangeTitleForm(String value) {
@@ -36,35 +37,34 @@ class CreateFormCubit extends Cubit<CreateFormState> {
   }
 
   void editQuestion(int index, String value) {
-    state.listFormData[index] =
-        state.listFormData[index].copyWith(question: value);
-    emit(state.copyWith(listFormData: state.listFormData.toList()));
+    state.questions[index] = state.questions[index].copyWith(question: value);
+    emit(state.copyWith(questions: state.questions.toList()));
   }
 
   void addOptionQuestion(int index, bool isAddOther) {
-    var list = state.listFormData.toList();
+    var list = state.questions.toList();
     var data = list[index];
     list[index] = list[index].copyWith(
         resultOption: data.resultOption.toList()
           ..add(isAddOther && !data.resultOption.contains("Others")
               ? "Others"
               : "Option ${data.resultOption.length + 1}"));
-    emit(state.copyWith(listFormData: list.toList()));
+    emit(state.copyWith(questions: list.toList()));
   }
 
   void changeOptionQuestion({required int index, required int indexSelected}) {
-    var list = state.listFormData.toList();
+    var list = state.questions.toList();
     var data = list[index];
     data = data.copyWith(indexSelected: indexSelected);
     list[index] = data;
-    emit(state.copyWith(listFormData: List.from(list)));
+    emit(state.copyWith(questions: List.from(list)));
   }
 
   void updateResultOption(
       {required int index,
       required int indexSelected,
       required String result}) {
-    var list = state.listFormData.toList();
+    var list = state.questions.toList();
     var data = list[index];
     var option = data.resultOption[indexSelected];
     option = result;
@@ -73,34 +73,34 @@ class CreateFormCubit extends Cubit<CreateFormState> {
 
     data = data.copyWith(resultOption: data.resultOption);
     list[index] = data;
-    emit(state.copyWith(listFormData: List.from(list)));
+    emit(state.copyWith(questions: List.from(list)));
   }
 
   void duplicateQuestion(int position) {
-    List<MQuestion> listUpdated = List.from(state.listFormData);
-    listUpdated.add(state.listFormData[position]);
-    emit(state.copyWith(listFormData: listUpdated));
+    List<MQuestion> listUpdated = List.from(state.questions);
+    listUpdated.add(state.questions[position]);
+    emit(state.copyWith(questions: listUpdated));
   }
 
   void isRequiredForm({required int position, required bool isRequired}) {
-    List<MQuestion> listUpdated = List.from(state.listFormData);
+    List<MQuestion> listUpdated = List.from(state.questions);
     listUpdated[position] = listUpdated[position].copyWith(
       isRequired: isRequired,
     );
-    emit(state.copyWith(listFormData: listUpdated));
+    emit(state.copyWith(questions: listUpdated));
   }
 
   void selectQuestionOption({required int position, required int value}) {
-    List<MQuestion> listUpdated = List.from(state.listFormData);
+    List<MQuestion> listUpdated = List.from(state.questions);
     listUpdated[position] =
         listUpdated[position].copyWith(optionQuestion: value);
-    emit(state.copyWith(listFormData: listUpdated));
+    emit(state.copyWith(questions: listUpdated));
   }
 
   void onChangeParagraph({required String value, required int position}) {
-    List<MQuestion> listUpdated = List.from(state.listFormData);
+    List<MQuestion> listUpdated = List.from(state.questions);
     listUpdated[position] =
         listUpdated[position].copyWith(resultParagraph: value);
-    emit(state.copyWith(listFormData: listUpdated));
+    emit(state.copyWith(questions: listUpdated));
   }
 }
